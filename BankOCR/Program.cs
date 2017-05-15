8af1d4918ae94c7a0f3ad0e55b9959ef1b9d8557
Gustavo Veloso <gveloso@comscore.com>
2017-05-15 19:22:11 -0400

    Second Iteration, but not working!!

--- BankOCR/Program.cs
+++ BankOCR/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,9 @@ namespace BankOCR
 {
     public class Program
     {
-        public static int ParseNumber(string input)
+        public static int ParseDigit(string input)
         {
-            var lines = input.Split(new[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries);
+            var lines = input.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
             if (lines[0] == "   ")
             {
                 if (lines[1] == "  |")
@@ -20,19 +21,54 @@ namespace BankOCR
                 {
                     return 4;
                 }
-                
-               
             }
-
             else
             {
-                if (lines[0] == " _ "&& lines[1] == "  |" && lines [2] =="  |")
+                if (lines[0] == " _ ")
                 {
-                    return 7;
+                    if (lines[1] == "| |")
+                        return 0;
+                    if (lines[1] == "  |" && lines[2] == "  |")
+                        return 7;
+                    if (lines[1] == " _|")
+                    {
+                        if (lines[2] == " _|")
+                            return 3;
+                        if (lines[2] == "|_ ")
+                            return 2;
+                    }
+                    if (lines[1] == "|_ ")
+                    {
+                        if (lines[2] == "|_|")
+                            return 6;
+                        return 5;
+                    }
+                    if (lines[1] == "|_|")
+                    {
+                        if (lines[2] == "|_|")
+                            return 8;
+                        if (lines[2] == " _|")
+                            return 9;
+                    }
                 }
 
             }
             throw new ArgumentException("Input is not a valid [0-9] range", nameof(input));
+        }
+
+        public static int BuildNumber(string input)
+        {
+            for (var d = 0; d < input.Length / 3;)
+            {
+                var lines = input.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                var digit = lines[d];
+                for (var i = 0; i < d+3; i++)
+                {
+                    //NO FUNCIONA, PENSAR MEJOR :)
+                }
+                d = d + 3;
+            }
         }
 
         static void Main(string[] args)
--- BankOCRTests/UnitTest1.cs
+++ BankOCRTests/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using BankOCR;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,39 +10,159 @@ namespace BankOCRTests
     [TestClass]
     public class UnitTest1
     {
+
+        private const string One = "   " + "\r\n" +
+                                   "  |" + "\r\n" +
+                                   "  |" + "\r\n" +
+                                   "   " + "\r\n";
+
+        private const string Two = " _ " + "\r\n" +
+                                   " _|" + "\r\n" +
+                                   "|_ " + "\r\n" +
+                                   "   " + "\r\n";
+
+        private const string Three = " _ " + "\r\n" +
+                                     " _|" + "\r\n" +
+                                     " _|" + "\r\n" +
+                                     "   " + "\r\n";
+
+        private const string Four = "   " + "\r\n" +
+                                    "|_|" + "\r\n" +
+                                    "  |" + "\r\n" +
+                                    "   " + "\r\n";
+
+        private const string Five = " _ " + "\r\n" +
+                                    "|_ " + "\r\n" +
+                                    " _|" + "\r\n" +
+                                    "   " + "\r\n";
+
+        private const string Six = " _ " + "\r\n" +
+                                   "|_ " + "\r\n" +
+                                   "|_|" + "\r\n" +
+                                   "   " + "\r\n";
+
+        private const string Seven = " _ " + "\r\n" +
+                                     "  |" + "\r\n" +
+                                     "  |" + "\r\n" +
+                                     "   " + "\r\n";
+
+        private const string Eight = " _ " + "\r\n" +
+                                     "|_|" + "\r\n" +
+                                     "|_|" + "\r\n" +
+                                     "   " + "\r\n";
+
+        private const string Nine = " _ " + "\r\n" +
+                                    "|_|" + "\r\n" +
+                                    " _|" + "\r\n" +
+                                    "   " + "\r\n";
+
+        private const string Zero = " _ " + "\r\n" +
+                                    "| |" + "\r\n" +
+                                    "|_|" + "\r\n" +
+                                    "   " + "\r\n";
+        
+        private string MultipleDigitNumber(params string[] digits)
+        {
+            var newNumber = new StringBuilder();
+            for (var i = 0; i <= 3; i++)
+            {                
+                foreach (var digit in digits)
+                {
+                    var lines = digit.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                    newNumber.Append(lines[i]);
+                }
+                newNumber.Append("\r\n");
+            }
+            return newNumber.ToString();
+
+        }
+
         [TestMethod]
         public void ParsingOne()
         {
-            var one = "   " + "\r\n" +
-                      "  |" + "\r\n" +
-                      "  |" + "\r\n" +
-                      "   " + "\r\n";
-
-            Assert.AreEqual(1, Program.ParseNumber(one));
+            Assert.AreEqual(1, Program.ParseDigit(One));
         }
 
 
         [TestMethod]
         public void ParsingFour()
         {
-     
-            var four ="   " + "\r\n" +
-                      "|_|" + "\r\n" +
-                      "  |" + "\r\n" +
-                      "   " + "\r\n";
 
-           Assert.AreEqual(4, Program.ParseNumber(four));
+            Assert.AreEqual(4, Program.ParseDigit(Four));
         }
 
+        [TestMethod]
         public void ParsingSeven()
         {
 
-            var seven =" _ " + "\r\n" +
-                       "  |" + "\r\n" +
-                       "  |" + "\r\n" +
-                       "   " + "\r\n";
 
-            Assert.AreEqual(7, Program.ParseNumber(seven));
+            Assert.AreEqual(7, Program.ParseDigit(Seven));
+        }
+
+        [TestMethod]
+        public void ParsingThree()
+        {
+
+            Assert.AreEqual(3, Program.ParseDigit(Three));
+        }
+
+        [TestMethod]
+        public void ParsingTwo()
+        {
+
+            Assert.AreEqual(2, Program.ParseDigit(Two));
+        }
+
+        [TestMethod]
+        public void ParsingFive()
+        {
+
+            Assert.AreEqual(5, Program.ParseDigit(Five));
+        }
+
+        [TestMethod]
+        public void ParsingEight()
+        {
+
+            Assert.AreEqual(8, Program.ParseDigit(Eight));
+        }
+
+        [TestMethod]
+        public void ParsingNine()
+        {
+
+            Assert.AreEqual(9, Program.ParseDigit(Nine));
+        }
+
+        [TestMethod]
+        public void ParsingZero()
+        {
+
+            Assert.AreEqual(0, Program.ParseDigit(Zero));
+        }
+
+        [TestMethod]
+        public void ParsingSix()
+        {
+
+            Assert.AreEqual(6, Program.ParseDigit(Six));
+        }
+
+        [TestMethod]
+        public void ParsingTen()
+        {
+            Assert.AreEqual(10, MultipleDigitNumber(One, Zero));
+        }
+
+        [TestMethod]
+        public void BuildTen()
+        {
+            var ten = "    _ " + "\r\n" +
+                      "  || |" + "\r\n" +
+                      "  ||_|" + "\r\n" +
+                      "      " + "\r\n";
+
+            Assert.AreEqual(ten, MultipleDigitNumber(One, Zero));
         }
     }
 }
